316f4c70873aa1b8cedbcde2f6883dacd4b11f01
Dreamcaster70 <147625950+Dreamcaster70@users.noreply.github.com>
2023-11-25 20:11:55 +0500

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,8 @@
+class Car: 
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+    def drive(self):
+        print(f'Driving the {self.make}, {self.model}')
+my_car = Car("Mercedes", "C-class")
+my_car.drive()
